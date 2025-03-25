07727d0d1b060bd044ae9e04e4181a2681d93755
MSRishab007 <s.rishab0807@gmail.com>
2025-03-26 01:36:51 +0530

    Remove Redundant files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import './App.css'
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Convert from './Pages/Convert';
-import Home from './Pages/Home';
 import SplinePage from './Pages/SplinePage';
-import LearnSign from './Pages/LearnSign';
-import Video from './Pages/Video';
 import Navbar from './Components/Navbar';
-import CreateVideo from './Pages/CreateVideo';
 import Footer from './Components/Footer';
-import Videos from './Pages/Videos';
-import Feedback from './Pages/Feedback';
 function App() {
   return (
     <Router>
@@ -21,11 +15,6 @@ function App() {
           <Route exact path="/sign-kit/" element={<Navigate to="/sign-kit/convert" />} />
           <Route exact path="/sign-kit/home" element={<SplinePage />} />
           <Route exact path="/sign-kit/convert" element={<Convert/>} />
-          <Route exact path="/sign-kit/learn-sign" element={<LearnSign />} />
-          <Route exact path="/sign-kit/all-videos" element={<Videos />} />
-          <Route exact path="/sign-kit/video/:videoId" element={<Video />} />
-          <Route exact path="/sign-kit/create-video" element={<CreateVideo />} />
-          <Route exact path="/sign-kit/feedback" element={<Feedback />} />
           <Route exact path="*" element={<Navigate to="/sign-kit/home" />} />
         </Routes>
         <Footer />
